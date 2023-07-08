e77d54ad5ea3c2940e1a52303f04061a4abeae99
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-07-08 14:10:02 +0000

    18

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'homes#top'
+  get 'homes/about', to: 'homes#about', as: 'about'
 
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+ resources :post_comments, only: [:create]
+end 
 resources :users, only: [:show, :edit, :update]
 
-get 'homes/about', to: 'homes#about', as: 'about'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
